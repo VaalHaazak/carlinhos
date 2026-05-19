2abe020a835e33819eac33f62153b0e459f72d96
VaalHaazak <paulorenan2345@gmail.com>
2026-05-19 10:26:29 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,35 @@
 import 'package:aula15/pages/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: HomePage()
     );
   }
 }
+
+/////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////
+
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,17 +1,73 @@
+import 'package:aula15/model/database.dart';
+import 'package:aula15/model/product.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _controller = TextEditingController();
+
+  int? selectedItem;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Aula15! vai tomando"),
-      ),
+      appBar: AppBar(title: TextField(controller: _controller)),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.save),
+        onPressed: () async {
+          print(_controller.text);
 
-      body: Center(
-        child: FlutterLogo(size: 200,),
+          selectedItem != null
+              ? await DatabaseHelper.instance.updateProduct(
+                  Product(id: selectedItem, name: _controller.text),
+                )
+              : await DatabaseHelper.instance.addProduct(
+                  Product(name: _controller.text),
+                );
+
+          setState(() {
+            selectedItem = null;
+            _controller.clear();
+          });
+        },
+      ),
+      body: FutureBuilder(
+        future: DatabaseHelper.instance.getProducts(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: Text("Carregando..."));
+          }
+          return snapshot.data!.isEmpty
+              ? Center(child: Text("Não há produtos na lista"))
+              : ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, i) {
+                    return ListTile(
+                      leading: Text(snapshot.data![i].id.toString()),
+                      title: Text(snapshot.data![i].name!),
+                      onLongPress: () {
+                        setState(() {
+                          DatabaseHelper.instance.removeProduct(
+                            snapshot.data![i].id!,
+                          );
+                        });
+                      },
+                      onTap: () {
+                        setState(() {
+                          selectedItem = snapshot.data![i].id!;
+                          _controller.text = snapshot.data![i].name!;
+                        });
+                      },
+                    );
+                  },
+                );
+        },
       ),
     );
   }
